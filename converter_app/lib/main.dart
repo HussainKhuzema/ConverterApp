a599a75fe54f9b42c8517d350be2957e64ca8474
Hussain <hussainkhuzema53@gmail.com>
2022-10-22 17:13:24 +0500

    removed debug banner

--- converter_app/lib/main.dart
+++ converter_app/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Converter',
     theme: ThemeData(
       primarySwatch: Colors.blueGrey,
